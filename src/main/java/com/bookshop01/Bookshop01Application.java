9ad360670b2eda8c8c0e0d1097c930d54e7973cf
sangin1 <tkwk9gh@naver.com>
2021-12-15 14:14:29 +0900

    upload

---
+++ src/main/java/com/bookshop01/Bookshop01Application.java
@@ -0,0 +1,13 @@
+package com.bookshop01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookshop01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bookshop01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/bookshop01/config/mybatisConfig.java
@@ -0,0 +1,38 @@
+package com.bookshop01.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+ 
+@Configuration
+@MapperScan(value= {"mybatis/mappers"})
+@EnableTransactionManagement
+public class mybatisConfig {
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{
+            SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+            sessionFactory.setDataSource(dataSource);
+            
+            Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mappers/*.xml");
+            
+            sessionFactory.setMapperLocations(res);
+            
+            return sessionFactory.getObject();
+    }
+ 
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
+ 
